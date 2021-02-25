613a8c8137ba4a4fbf12456d33aed0acd4e10049
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2021-02-25 14:31:59 -0500

    finish

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+#/etc/default/nginx -- ULIMIT="-n 15" to ULIMIT="-n 4096"
+
+file { 'replace last line':
+    ensure  => present,
+    path    => '/etc/default/nginx',
+    content => 'ULIMIT="-n 4096"',
+}
+
+service { 'nginx':
+    ensure    => running,
+    subscribe => File['/etc/default/nginx']
+}
